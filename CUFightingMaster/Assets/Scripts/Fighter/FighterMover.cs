b978f7a99bcae8efd3d4c84aacf2c15086cd71c5
kazuma <17cu0235@gmail.com>
2019-07-05 03:03:03 +0900

    移動するまで

--- CUFightingMaster/Assets/Scripts/Fighter/FighterMover.cs
+++ CUFightingMaster/Assets/Scripts/Fighter/FighterMover.cs
@@ -60,50 +60,56 @@ public class FighterMover
 			gravityFrame = 0;
 		}
 	}
-	#endregion
-	#region 移動
-	//移動
-	private void MovementSkill()
-	{
-		if ((moves == null) || (moves.Count == 0)) return;//nullチェック
-		//次があるかどうか
-		if (moves.Count > nowPlayMoveNumber + 1)
-		{
-			//現在再生中の移動の次の移動フレームを越えれば
-			if (moves[nowPlayMoveNumber + 1].startFrame <= core.AnimationPlayerCompornent.NowFrame)
-			{
-				nowPlayMoveNumber++;
-			}
-		}
-		if (nowPlayMoveNumber < 0) return;//-1なら動かない
-		//移動
-		transform.Translate(moves[nowPlayMoveNumber].movement*0.1f);
-	}
-	//重力移動
-	private void GravityMovementSkill()
-	{
-		if ((gravity == null) || (gravity.Count == 0)) return;
-		if (gravity.Count > nowPlayGravityNumber + 1)
-		{
-			//現在再生中の移動の次の移動フレームを越えれば
-			if(gravity[nowPlayGravityNumber+1].startFrame<=core.AnimationPlayerCompornent.NowFrame)
-			{
-				nowPlayGravityNumber++;
-			}
-		}
-		if (nowPlayGravityNumber < 0) return;
-		//移動保存用
-		Vector3 move = gravity[nowPlayGravityNumber].movement * gravityFrame;
-		if (Mathf.Abs(move.x) > Mathf.Abs(gravity[nowPlayGravityNumber].limitMove.x))
-		{
-			move.x = gravity[nowPlayGravityNumber].limitMove.x;
-		}
-
-		if (Mathf.Abs(move.y) > Mathf.Abs(gravity[nowPlayGravityNumber].limitMove.y))
-		{
-			move.y = gravity[nowPlayGravityNumber].limitMove.y;
-		}
-		transform.Translate(move*0.1f);
-	}
-	#endregion
+    #endregion
+    #region 移動
+    //移動
+    private void MovementSkill()
+    {
+        if ((moves == null) || (moves.Count == 0)) return;//nullチェック
+                                                          //次があるかどうか
+        if (moves.Count > nowPlayMoveNumber + 1)
+        {
+            //現在再生中の移動の次の移動フレームを越えれば
+            if (moves[nowPlayMoveNumber + 1].startFrame <= core.AnimationPlayerCompornent.NowFrame)
+            {
+                nowPlayMoveNumber++;
+            }
+        }
+        if (nowPlayMoveNumber < 0) return;//-1なら動かない
+        int xDirection = 1;
+        if (core.Direction == PlayerDirection.Left) xDirection = -1;
+        Vector3 move = moves[nowPlayMoveNumber].movement;
+        move.x *= xDirection;
+        //移動
+        transform.Translate(move * 0.1f);
+    }
+    //重力移動
+    private void GravityMovementSkill()
+    {
+        if ((gravity == null) || (gravity.Count == 0)) return;
+        if (gravity.Count > nowPlayGravityNumber + 1)
+        {
+            //現在再生中の移動の次の移動フレームを越えれば
+            if (gravity[nowPlayGravityNumber + 1].startFrame <= core.AnimationPlayerCompornent.NowFrame)
+            {
+                nowPlayGravityNumber++;
+            }
+        }
+        if (nowPlayGravityNumber < 0) return;
+        //移動保存用
+        Vector3 move = gravity[nowPlayGravityNumber].movement * gravityFrame;
+        int xDirection = 1;
+        if (core.Direction == PlayerDirection.Left) xDirection = -1;
+        if (Mathf.Abs(move.x) > Mathf.Abs(gravity[nowPlayGravityNumber].limitMove.x))
+        {
+            move.x = gravity[nowPlayGravityNumber].limitMove.x;
+        }
+        if (Mathf.Abs(move.y) > Mathf.Abs(gravity[nowPlayGravityNumber].limitMove.y))
+        {
+            move.y = gravity[nowPlayGravityNumber].limitMove.y;
+        }
+        move.x *= xDirection;
+        transform.Translate(move * 0.1f);
+    }
+    #endregion
 }
